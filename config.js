3db78fdb12c2a610ef9309aebcdb6df6f652edda
Forexmd77777 <Sevindaihara@gmail.com>
2025-08-16 15:23:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KSMD~fwBD2RwI#WVSwauBEgngWTEy9ThdBfs270DTrztUY_7_huKCTBVA",
+SESSION_ID: process.env.SESSION_ID || "KSMD~9VhxxaTJ#z1UIanW3u37DUJMCZnfcKsbSD-RtkDIyrJbMbaz6B8k",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
